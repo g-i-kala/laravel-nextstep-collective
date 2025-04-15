e06d32df9239dd6e8fe65ae06ff8ce053bc79e04
g-i-kala <5star@wp.pl>
2025-04-15 20:37:38 +0200

    feat: added unassigned Tags Pruning

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,16 +5,23 @@ namespace App\Models;
 use App\Models\Job;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
     /** @use HasFactory<\Database\Factories\TagFactory> */
     use HasFactory;
+    use Prunable;
 
     public function jobs(): BelongsToMany
     {
         return $this->belongsToMany(Job::class);
     }
-    
+
+    public function prunable()
+    {
+        return $this->doesntHave('jobs');
+    }
+
 }
