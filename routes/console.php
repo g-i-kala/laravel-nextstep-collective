f7e737447ad8826b39019ea2b10eecfdab71af17
g-i-kala <5star@wp.pl>
2025-04-17 13:10:46 +0200

    fets: prune schedule for tag added

--- routes/console.php
+++ routes/console.php
@@ -1,10 +1,15 @@
 <?php
 
-use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Foundation\Console\ClosureCommand;
 
 Artisan::command('inspire', function () {
     /** @var ClosureCommand $this */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+
+Schedule::command('model:prune --model="App\Models\Tag"')->daily();
