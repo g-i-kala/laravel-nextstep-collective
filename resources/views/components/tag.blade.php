f0a85040ebf1444f275b26b80cab47cc338a53d5
g-i-kala <5star@wp.pl>
2025-06-03 16:43:12 +0200

    style: tag border

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,13 +1,14 @@
 @props(['tag', 'size' => 'base'])
 
 @php
-    $classes = 'bg-border/30 hover:bg-border rounded-b-xl font-bold transition-colors duration-300';
+    $classes =
+        'border border-gray-300 bg-border/30 hover:bg-border rounded-b-xl font-bold transition-colors duration-300';
     if ($size === 'base') {
         $classes .= ' px-5 py-1 text-sm';
     }
 
     if ($size === 'small') {
-        $classes .= ' px-3 py-1 text-2xs';
+        $classes .= ' px-3 py-1 text-xs';
     }
 @endphp
 
